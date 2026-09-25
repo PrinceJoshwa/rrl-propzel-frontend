306717742cad729a3dd702dac3681617450c58a7
PrinceJoshwa <sprincejoshwa85@gmail.com>
2026-09-25 13:51:16 +0530

    fix: build error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { ProjectProvider } from "@/contexts/ProjectContext";
 import { OrganizationProvider } from "@/contexts/OrganizationContext";
 import { Toaster } from "@/components/ui/sonner";
 
-import Login from "@/pages/Logi";
+import Login from "@/pages/Login";
 import AppLayout from "@/components/layout/AppLayout";
 import Dashboard from "@/pages/Dashboard";
 import Leads from "@/pages/Leads";
